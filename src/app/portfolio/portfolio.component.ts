848feff52eee49eed2f40f9d3e3d672b323722b5
lamahewageDulani <dulaniruwanthika99@gmail.com>
2024-02-03 21:34:25 +0530

    project info

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  projectsArray: {name: string, topic: string, description: string, image: string, link: string, btnname: string}[] = [
+    {
+      name:'Web Design',
+      topic:'Employee Performance Monitoring System.',
+      description:'With the collaboration of five members team, we developed this system for the second year project of the university. This system is developed using ReactJS, Spring Boot and MySQL.',
+      image:'./assets/images/portfolio-1.jpg',
+      link: 'https://github.com/TrackoApplication',
+      btnname: 'GitHub Link'
+    },
+    {
+      name:'Hardware Project',
+      topic:'Coconot Breaking, Scraping and Grinding machine',
+      description:'With the collaboration of five members team, we developed this virtual machine for the first year project of the university. This machine is developed using C#, Atmel Studio and other related software.',
+      image:'./assets/images/portfolio-2.jpg',
+      link: '#',
+      btnname:'View Project'
+    }
+  ];
+
 }
